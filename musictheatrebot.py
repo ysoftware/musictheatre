80fe341bb3ac15ef83fc6ba3cbeffcd56da3f714
Yaroslav Erohin <ysoftware@yandex.ru>
2017-03-04 01:35:08 +0300

    ok

--- musictheatrebot.py
+++ musictheatrebot.py
@@ -2,12 +2,12 @@ from telegram.ext import Updater, CommandHandler
 import sched, time, random, logging, pickle
 
 watb = "-1001049406492"
+admins = ["ysoftware", "frederik81", "tbshfmn",  "sexy_nutella_69", "amobishoproden"]
 
 # access
 
 def checkAccess(update):
-    names = ["ysoftware", "frederik81", "tbshfmn",  "sexy_nutella_69", "amobishoproden"]
-    return update.message.from_user.username in names
+    return update.message.from_user.username in admins
 
 # session
 
@@ -32,7 +32,7 @@ def newAlbum(bot, update):
         return
     config = loadConfig()
     if config[0] == False:
-        message = update.message.text.split(" ", 1)[1].strip()
+        message = update.message.text.split(" ", 1)[1].encode('utf-8').strip()
         artistName = message.split(" - ", 1)[0].strip()
         albumName = message.split(" - ", 1)[1].strip()
 
@@ -50,8 +50,8 @@ def nextSong(bot, update):
         return
     config = loadConfig()
     if config[0] == True:
-        trackName = update.message.text.split(" ", 1)[1].strip()
-        if len(trackName) > 2 and len(config[1]) > 2:
+        trackName = update.message.text.split(" ", 1)[1].encode('utf-8').strip()
+        if len(trackName) > 2 and len(config[1]) > 2 and trackName != config[3]:
             config[3] = trackName
             text = "#musictheatre {0} - {1}".format(config[1], config[3])
             bot.sendMessage(watb, text.encode('utf-8'))
@@ -145,8 +145,6 @@ def roll(bot, update):
 # suggest
 
 def suggest(bot, update):
-    if not checkAccess(update):
-        return
     config = loadConfig()
     if config[0] == False:
         bot.sendMessage(watb, "<b>Anyone in for a </b>#musictheatre<b> session?</b>", parse_mode="HTML")
@@ -183,12 +181,14 @@ def randomWorstTime():
                           "{0} The Universe hasn't seen such <b>WORST TIME</b> in its whole <b>{1} HISTORY</b> of time! <b>EVER</b>".format(rfn(), rfa()),
                           "LITERALLY THE WORST TIMING POSSIBLE",
                           "NO NO NO NO STOP THAT NOT NOW",
-                          "NOOO {} NOT NOW, WE'RE BUSY!!!!".format(rfn()),
+                          "NOOO FOR {}'S SAKE, NOT NOW, WE'RE BUSY!!!!".format(rfn()),
                           "FUCK YOU WITH YOUR {0} LINKS YOU {1}".format(rfa(), rfn())
                          ])
 
 def randomCunt():
     return random.choice(["Turn off your fucking shuffle, I'ma cunt!",
+                          "OK! Shut the fuck up, I'm the boss in here!",
+                          "Shut up and Dance! In...",
                           "Are you bitches ready? Starting the coundoooown!",
                           "Ready Lets Go",
                           "Everybody SHUT UP and listen!"])
@@ -225,6 +225,11 @@ def randomMango(name):
                           "<i>{} is jealous of the fucking deaf</i>.".format(name)
                           ])
 
+# help
+
+def help(bot, update):
+    update.message.reply_text("Here's the list of available commands:\n<b>/spreadshit</b> gives you the link to our spreadshit\n<b>/suggest</b> bot will ask if anyone wants to start a session\n<b>/admins</b> for the list of people who can control this bot\n\nUse these while in session:\n<b>/song</b> or <b>/album</b> to find out what's playing\n<b>/time</b> in case of like the worst time ever\n<b>/mango</b> to express your feelings about the current song".encode('utf-8'), parse_mode="HTML")
+
 # say something
 
 def say(bot, update):
@@ -235,27 +240,50 @@ def say(bot, update):
     message = update.message.text.split(" ", 1)[1].strip()
     bot.sendMessage(watb, message.encode('utf-8'), parse_mode="HTML")
 
+# admins
+
+def adminList(bot, update):
+    text = "These <i>(%username%)</i>s have access to the bot's #musictheatre session commands:\n"
+    for name in admins:
+        text += "- " + name + "\n"
+    text += "If they are not around, God help you."
+    update.message.reply_text(text.encode('utf-8'), parse_mode="HTML")
+
+# spreadshit link
+
+def shit(bot, update):
+    update.message.reply_text("Here's the link to our spreadshit: http://bit.ly/watb-spreadshit")
+
 # work
 
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 updater = Updater('337143431:AAHK2PvoU6-HV5EJb6ydlCGzlvnqj8YFFVs')
 
 # general commands
 
-updater.dispatcher.add_handler(CommandHandler('roll', roll))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('admins', adminList))
+
+updater.dispatcher.add_handler(CommandHandler('shit', shit))
+updater.dispatcher.add_handler(CommandHandler('sheet', shit))
+updater.dispatcher.add_handler(CommandHandler('spreadshit', shit))
+updater.dispatcher.add_handler(CommandHandler('spreadsheet', shit))
+
+updater.dispatcher.add_handler(CommandHandler('suggest', suggest))
 updater.dispatcher.add_handler(CommandHandler('mango', mango))
 updater.dispatcher.add_handler(CommandHandler('time', badTiming))
 updater.dispatcher.add_handler(CommandHandler('song', currentTrack))
 updater.dispatcher.add_handler(CommandHandler('album', currentAlbum))
 
-# session control
+# admin commands
 
-updater.dispatcher.add_handler(CommandHandler('suggest', suggest))
+updater.dispatcher.add_handler(CommandHandler('roll', roll))
 updater.dispatcher.add_handler(CommandHandler('cunt', cunt))
 updater.dispatcher.add_handler(CommandHandler('new', newAlbum))
 updater.dispatcher.add_handler(CommandHandler('n', nextSong))
 updater.dispatcher.add_handler(CommandHandler('abort', abort))
 updater.dispatcher.add_handler(CommandHandler('over', over))
+
 updater.dispatcher.add_handler(CommandHandler('b', say))
 
 updater.start_polling()
