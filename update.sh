995d9c3e646835cd756ccf69644dbbffc34fb710
Yaroslav Erokhin <ysoftware@yandex.ru>
2019-09-29 15:21:02 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-ls &&
-cd /usr/local/tgbots/musictheatrebot &&
+echo "1" &&
 systemctl stop musictheatrebot &&
+echo "2" &&
+cd /usr/local/tgbots/musictheatrebot &&
+ls &&
 git pull &&
+echo "3" &&
 systemctl start musictheatrebot
