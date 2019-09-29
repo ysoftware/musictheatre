a022c069264cac4f9aafb7cabfc26f877cb9a4b5
Yaroslav Erokhin <ysoftware@yandex.ru>
2019-09-29 15:08:30 +0300

    Update other_commands.py

--- other_commands.py
+++ other_commands.py
@@ -153,7 +153,7 @@ def update(bot, update):
         return
     if update.message.from_user.username == "ysoftware":
         bot.sendMessage(101193334, "Running...")
-        out = subprocess.check_output("/usr/local/tgbots/musictheatrebot/update.sh")
+        out = subprocess.check_output(["/usr/local/tgbots/musictheatrebot/update.sh"])
         bot.sendMessage(101193334, out)
         bot.sendMessage(101193334, "Done")
 
