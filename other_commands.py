a0dd723e9d15f1d938c732496eb52ba5028dd21d
riodevelop <a.volodjaev@gmail.com>
2019-11-05 19:55:34 +0300

    Update other_commands.py
    Fixed 8ball

--- other_commands.py
+++ other_commands.py
@@ -86,7 +86,7 @@ def russian(bot, update):
 
 def randomBall():
 #       return random.choice(["It is certain", "It is decidedly so", "Without a doubt"])
-        return random.choice(["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely", "You may rely on it", "As I see it, yes", "Most li$
+        return random.choice(["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Signs point to yes", "Yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"})
 def ball(bot, update):
         if not isNewCommand(update):
                 message = randomBall()
