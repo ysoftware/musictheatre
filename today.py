a6346c188ac4a6cc275d59e0d941d4caef7acfaa
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-11-06 23:00:52 +0300

    Update today.py

--- today.py
+++ today.py
@@ -10,11 +10,11 @@ import json
 URL = 'https://history.muffinlabs.com/date'
 
 def load_today():
-    return json.loads(requests.get(URL).text)
+	return json.loads(requests.get(URL).text)
 
 def renew():
 
-    today_json = load_today()
+	today_json = load_today()
 
 	today_out = ('')
 
@@ -28,23 +28,23 @@ def renew():
 					today_out = today_out + ('In year '+event['year']+'-'+event['text']+'\n')
 
 	for birth in today_json['data']['Births']:
-        	choice = random.randint(0,45)
-        	if choice == 1:
-            	  	if birth['year'] is not None:
-                        	if birth['text'] is not None:
-                			today_out = today_out + (birth['text']+' was born in '+birth['year']+'\n')
+			choice = random.randint(0,45)
+			if choice == 1:
+					if birth['year'] is not None:
+							if birth['text'] is not None:
+							today_out = today_out + (birth['text']+' was born in '+birth['year']+'\n')
 
 	for death in today_json['data']['Deaths']:
-        	choice = random.randint(0,25)
-        	if choice == 1:
+			choice = random.randint(0,25)
+			if choice == 1:
 			if death['year'] is not None:
-                		if death['text'] is not None:
-               				today_out = today_out + ('In year '+death['year']+' died '+death['text']+'\n')
+						if death['text'] is not None:
+							today_out = today_out + ('In year '+death['year']+' died '+death['text']+'\n')
 
 	return today_out
 
 def today(bot, update):
-        if not isNewCommand(update):
+		if not isNewCommand(update):
 		return
 	message = renew()
 	send(bot, message.encode('utf-8'))
