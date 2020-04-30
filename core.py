0ad2a99b4606830488abe0d3e62413404b68cb5c
Yaroslav Erohin <ysoftware@yandex.ru>
2020-04-30 16:47:27 +0300

    debug mode

--- core.py
+++ core.py
@@ -2,6 +2,8 @@ import datetime, numpy
 
 watb = -1001049406492
 newseeds = -1001138132564
+bot_test = -329730756
+debug = True # DEBUG ONLY # TODO: PARSE FROM ARGUMENT
 
 devs = list(map(lambda x: x.rstrip('\n'), open('devs_names.txt', 'r').readlines()))
 admins = list(map(lambda x: x.rstrip('\n'), open('admins_names.txt', 'r').readlines()))
@@ -29,8 +31,12 @@ def isNewCommand(update):
     value = dt < (20000)
     return value
 
+def main_channel(): 
+    if debug: return bot_test
+    else: return newseeds
+
 def isNewSeeds(update):
-    return update.message.chat_id == newseeds
+    return update.message.chat_id == main_channel()
 
 def getWeights(count):
     values = list(range(0, count))
