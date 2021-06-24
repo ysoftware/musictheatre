f0f391d9991eea9c5986e8d5a444840807159c28
Iaroslav Erokhin <iaroslav.erokhin@check24.de>
2021-06-24 14:32:27 +0200

    edited text of /ball

--- other_commands.py
+++ other_commands.py
@@ -77,7 +77,7 @@ def ball(bot, update):
 
     message = random.choice(["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitely", 
     "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Signs point to yes", "Yes", 
-    "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", 
+    "All I see is fog", "Ask again later", "Better not tell you now", "Cannot predict now", 
     "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", 
     "Outlook not so good", "Very doubtful"])
 
